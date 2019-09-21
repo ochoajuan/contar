6a7390479f0561a974227a1bc2b66206e0bd60c9
Juan David Ochoa <ochoajuan@outlook.com>
2019-09-21 06:52:24 -0500

    contar client v 0.1

---
+++ app/src/main/java/com/shadowspro/contar/data/LoginDataSource.kt
@@ -0,0 +1,27 @@
+package com.shadowspro.contar.data
+
+import com.shadowspro.contar.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+
+            //code of fake user response
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Juan David")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
+
---
+++ app/src/main/java/com/shadowspro/contar/data/model/TokenSuccess.kt
@@ -0,0 +1,5 @@
+package com.shadowspro.contar.data.model
+
+class TokenSuccess {
+    var token: List<String>? = null
+}
